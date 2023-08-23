fd1b85d861afd822fc5303bad27ea3e9ea314766
Gaurav-2k1 <gauravbhukte1507@gmail.com>
2023-08-23 14:27:16 +0530

    fix

--- src/components/dashboard/TemplateDrawer.jsx
+++ src/components/dashboard/TemplateDrawer.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Tempone from "../../assets/temp/tempone.png"
 import Temptwo from "../../assets/temp/temptwo.png"
 import Tempthree from "../../assets/temp/tempthree.png"
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setTemplate } from '../../slices/templateSlice'
 const TemplateDrawer = () => {
     return (
         <div className='absolute top-10 right-0
           z-50 delay-110 transition-all ease-linear'>
-            <div className='flex flex-col  overflow-y-auto p-2 bg-white min-h-[100px] '>
+            <div className='flex flex-col  overflow-y-auto p-2 bg-white min-h-[100px] max-h-[40vh]'>
 
                 <TemplateBox url={Tempone} link="/template/tempone" />
                 <TemplateBox url={Temptwo} link="/template/temptwo" />
@@ -22,14 +22,16 @@ const TemplateDrawer = () => {
 
 const TemplateBox = ({ url, link }) => {
     const dispatch = useDispatch()
+    const { template } = useSelector((state) => state.template)
 
 
     return (
-        <div className='w-[300px] h-[150px] bg-red-600 border-lg my-4  hover:brightness-50 delay-110 ease-in transition-all cursor-pointer'
+        <div className={`w-[300px] h-[200px] border-lg my-4  delay-110 ease-in transition-all cursor-pointer relative ${template === link ? "border-4 border-icob  border-solid":""}`}
             onClick={() => {
                 dispatch(setTemplate(link))
             }}>
-            <img src={url} alt='' className='object-cover' />
+            <img src={url} alt='' className='object-cover  group hover:brightness-50' />
+            <p className='bg-white text-black rounded-2xl px-3 py-1 absolute top-1/2 left-[40%] hidden group-hover:block delay-110 transition-all ease-in'>use </p>
         </div>
     )
 }
